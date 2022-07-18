ac11badca2163c4280087732b5c321fdebbad43e
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-07-18 15:02:59 +0300

    Header desktop

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,52 @@
 import Logo from '../images/LOGO.jpeg' 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+// import { useHistory } from 'react';
 
 export default function Header() {
+    const {pathname} = useLocation();
+
     return (
-        <header className="header">
-            <NavLink to="/Fond">
-                <img src={Logo} alt="Logo" width="50" height="50" />
-            </NavLink>
+        <header className={pathname === '/Fond' ? "header-home" : "header"}>
             <nav className='nav'>
-                <ul>
-                    <li className='nav_link'><NavLink to="/Fond">
+                <div className='container-header'>
+                <NavLink to="/Fond">
+                <img src={Logo} alt="Logo" width="175,5" height="28,5" />
+            </NavLink>
+                
+            
+                <div className='container-nav'>
+                <ul className='nav-list'>
+                    <li className={pathname === '/Fond' ? 'nav_link--home' : 'nav-link'}><NavLink to="/Fond" className={({ isActive }) =>
+                      `${isActive ? "active" : "nav_item"}`
+                    }>
                     Головна
                     </NavLink></li>
-                     <li className='nav_link'><NavLink to="/Help">
+                     <li className={pathname === '/Fond' ? 'nav_link--home' : 'nav-link'}><NavLink to="/Help" className={({ isActive }) =>
+                      `${isActive ? "active" : "nav_item"}`
+                    }>
                     Допомога
                     </NavLink></li>
-                     <li className='nav_link'><NavLink to="/Questions">
+                     <li className={pathname === '/Fond' ? 'nav_link--home' : 'nav-link'}><NavLink to="/Questions" className={({ isActive }) =>
+                      `${isActive ? "active" : "nav_item"}`
+                    }>
                     Часті запитання
                     </NavLink></li>
-                    <li className='nav_link'><NavLink to="/Support">
+                    <li className={pathname === '/Fond' ? 'nav_link--home' : 'nav-link'}><NavLink to="/Support" className={({ isActive }) =>
+                      `${isActive ? "active-support" : "nav_item-support"}`
+                    }>
                     Підтримати
                     </NavLink></li>
-                    <li className='nav_link'>
-                        <a href="tel:+380 63 258 3366">+380 63 258 3366
+                    <li className={pathname === '/Fond' ? 'nav_link--home' : 'nav-link'}>
+                        <a className='phone' href="tel:+380 63 258 3366">+380 63 258 3366
                     </a>
-                    </li>
+                        </li>
+                        <button className={pathname === '/Fond' ? 'nav_change-language--home' : 'nav_change-language'}>UA</button>
                 </ul>
+                </div>
+                </div>
                 
                 
-                <button>UA</button>
+                
             </nav>
             
         </header>
