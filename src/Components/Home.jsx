e080497644b1bca8e5810ad25ad23c3b00d53caf
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-07-21 17:02:05 +0300

    Fix foto

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -41,26 +41,28 @@ export default function Home() {
         </div>
         <div className="foto-about-sub_container">
           <img src={foto1} alt="results" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-            magni, molestias sequi voluptatum libero minus iste, error labore
-            fugiat in at deserunt vitae! Quae quasi, ea dolore accusantium
-            voluptatibus inventore. Voluptatem eveniet ipsa maiores. Quod iure
-            ea obcaecati ipsa nobis nesciunt illum perferendis distinctio odio.
-            Asperiores nemo, et atque illum, obcaecati voluptatibus nam eius
-            facere inventore possimus numquam sunt incidunt! Neque, error
-            commodi saepe architecto placeat eos corporis incidunt deleniti,
-            eligendi dicta, voluptas voluptate quo unde officiis porro nam
-            illum. Odio accusamus est labore, culpa sed necessitatibus a
-            reiciendis maiores? Nisi tempora eligendi suscipit debitis quidem ad
-            error magni at, excepturi aperiam assumenda impedit iusto deleniti
-            hic molestiae quod quis labore ratione dolorem asperiores
-            distinctio. Saepe quos eum perspiciatis aperiam. Repudiandae sunt
-            laborum quae, distinctio dolorum adipisci. Autem numquam ea
-            explicabo amet ex eligendi fugiat eveniet veritatis debitis
-            cupiditate. Praesentium impedit nulla et. Nemo, iusto excepturi
-            magni voluptatum aut quidem.
-          </p>
+          <div className="qwe">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              magni, molestias sequi voluptatum libero minus iste, error labore
+              fugiat in at deserunt vitae! Quae quasi, ea dolore accusantium
+              voluptatibus inventore. Voluptatem eveniet ipsa maiores. Quod iure
+              ea obcaecati ipsa nobis nesciunt illum perferendis distinctio
+              odio. Asperiores nemo, et atque illum, obcaecati voluptatibus nam
+              eius facere inventore possimus numquam sunt incidunt! Neque, error
+              commodi saepe architecto placeat eos corporis incidunt deleniti,
+              eligendi dicta, voluptas voluptate quo unde officiis porro nam
+              illum. Odio accusamus est labore, culpa sed necessitatibus a
+              reiciendis maiores? Nisi tempora eligendi suscipit debitis quidem
+              ad error magni at, excepturi aperiam assumenda impedit iusto
+              deleniti hic molestiae quod quis labore ratione dolorem asperiores
+              distinctio. Saepe quos eum perspiciatis aperiam. Repudiandae sunt
+              laborum quae, distinctio dolorum adipisci. Autem numquam ea
+              explicabo amet ex eligendi fugiat eveniet veritatis debitis
+              cupiditate. Praesentium impedit nulla et. Nemo, iusto excepturi
+              magni voluptatum aut quidem.
+            </p>
+          </div>
         </div>
       </div>
     </>
